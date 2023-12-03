781d4f5e6f321abf7389212969b6155af1a69446
Meerisha <57549724+Meerisha@users.noreply.github.com>
2023-12-02 19:16:52 -0600

    Add files via upload

---
+++ SkillHub.java
@@ -0,0 +1,7 @@
+public class SkillHub {
+public static void main(String[]args){
+System.out.println("SkillHub is a dynamic and innovative online marketplace that connects skilled professionals with individuals and businesses in need of their expertise.\n" +
+        "Our platform serves as a hub for freelancers, entrepreneurs, and businesses looking to access a wide range of talented individuals across various industres!");
+}
+}
+
